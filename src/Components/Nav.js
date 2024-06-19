3f8c4a73016f8222403628afdb6e8f9b1602d2cb
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-06-19 10:22:20 +0530

    update navbar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -8,21 +8,11 @@ import "../assets/css/nav.css";
 function Nav() {
   return (
     <nav className="nav-area">
-
       <ul className="nav-links">
         <li className="center">
           <Link to="/">
             <b>SmoothGulp</b>
           </Link>
-        </li>
-        <li className="search-bar">
-          <div className="search-input-container">
-            <input
-              type="text"
-              className="search-input"
-              placeholder={`   ${String.fromCodePoint(0x1F50D)} Search`}
-            />
-          </div>
         </li>
         <li className="logo">
           <Link to="">
