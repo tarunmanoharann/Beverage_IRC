5f73dc841bb2d69d41d60aa48ca5c29706426d50
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-06-24 14:39:19 +0530

    smoothgulp 3.0

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,57 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Img1 from '../assets/images/Tea.jpg';
 import Img2 from '../assets/images/coffee.jpg';
 import Img3 from '../assets/images/softdrinks.jpg';
 import Img4 from '../assets/images/wine.jpg';
 import Img5 from '../assets/images/whisky.jpg';
 import '../assets/css/card.css';
-import { Link } from 'react-router-dom';
+
+const products = [
+  { name: 'Tea', image: Img1, link: '/tea' },
+  { name: 'Coffee', image: Img2, link: '/coffee' },
+  { name: 'Soft Drinks', image: Img3, link: '/softdrinks' },
+  { name: 'Wine', image: Img4, link: '/wine' },
+  { name: 'Whisky', image: Img5, link: '/whisky' },
+];
 
 function Card() {
   return (
     <div id="scene">
-      <div id="left-zone">
-        <ul className="list">
-          <li className="item">
-            <input type="radio" id="first" name="basic_carousel" value="first" checked />
-            <label className="label_one" htmlFor="first">Showcase</label>
-            <div className="content content_one">
-              <div className="cardcontainer">
-                <Link to="/tea" className='card' style={{opacity:'0.9'}}>
-                  <h3 className="card-title">Tea</h3>
-                  <img src={Img1}  alt='img'/>
-                </Link>
-
-                <Link to="/coffee" className="card" style={{opacity: '0.9' }}>
-                  <h3 className="card-title">Coffee</h3>
-                  <img src={Img2} alt='img'/>
-                </Link>
-
-                <Link to="/softdrinks" className="card" style={{opacity: '0.9' }}>
-                  <h3 className="card-title">Soft Drinks</h3>
-                  <img src={Img3} alt='img'/>
-                </Link>
-
-                <Link to="/wine" className="card" style={{opacity: '0.9' }}>
-                  <h3 className="card-title">Wine</h3>
-                  <img src={Img4} alt='img'/>
-                </Link>
-
-                <Link to="/whisky" className="card" style={{opacity: '0.9' }}>
-                  <h3 className="card-title">Whisky</h3>
-                  <img src={Img5} alt='img'/>
-                </Link>
-                
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div id="middle-border"></div>
-      <div id="right-zone"></div>
+      <ul className="list">
+        <li className="item">
+          <input type="radio" id="first" name="basic_carousel" value="first" defaultChecked />
+          <div className="cardcontainer">
+            {products.map((product, index) => (
+              <Link key={index} to={product.link} className='card'>
+                <img src={product.image} alt={product.name} />
+                <h3 className="card-title">{product.name}</h3>
+              </Link>
+            ))}
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
-
 
 export default Card;
